5dadaf838633144f65d54c58a448384f643947fe
Josh Overly and Jon Sweeney <joshoverly@yahoo.com>
2016-01-19 08:58:18 -0800

    add functionality to the program

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var leapYear = function(year) {
+  if(year > 0 && year !== false) {
+    if((year % 400 === 0) || (year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  };
+};
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
